9a527f60db2ae853b86a7b3fcf42bfaafde151b3
parzavel-7 <surajganesh404@gmail.com>
2026-05-03 18:41:13 +0545

    feat: add Header component and include resume file for portfolio display

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -72,7 +72,7 @@ const Header = ({ isDarkMode, setIsDarkMode }) => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.3, delay: 0.42 }}
-          href="/my_resume.pdf"
+          href="/Suraj_Ganesh_Resume.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-white dark:bg-[#1c1c1c]"
         >
